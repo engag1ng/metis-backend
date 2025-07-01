3e8897dd271793864017d75081364c58ff37144b
engag1ng <131041909+engag1ng@users.noreply.github.com>
2025-07-02 07:28:54 +1000

    fixed authentication

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 # https://metis-backend.onrender.com/
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from supabase_api import create_supabase_client
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 app = FastAPI()
 supabase = create_supabase_client()
-
 
 @app.post("/account")
 def create_user(email, password):
     response = supabase.auth.sign_up({"email": email, "password": password,})
     return {
         "session": result.session.access_token,
+        "user": result.user
+    }
+@app.get("/account")
+def login_user(email: str, password: str):
+    result = supabase.auth.sign_in_with_password({"email": email, "password": password})
+    supabase.auth.set_session(result.session.access_token, result.session.refresh_token)
+    return {
+        "token": result.session.access_token,
         "user": result.user
     }
 
@@ -26,13 +34,3 @@ def new_simulation(user_id: str, start_date: str, end_date: str, start_capital: 
     }).execute()
 
     return response
-
-@app.get("/account")
-def login_user(email: str, password: str):
-    result = supabase.auth.sign_in_with_password({"email": email, "password": password})
-    session = result.session
-
-    return {
-        "token": result.session.access_token,
-        "user": result.user
-    }
